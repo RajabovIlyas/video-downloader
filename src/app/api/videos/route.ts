78ad1a0c7410152a05f4ed31c69411ab350fa7ac
RajabovIlyas <ilyasrajabow@gmail.com>
2024-05-27 09:56:15 +0500

    added error message for bad request

--- src/app/api/videos/route.ts
+++ src/app/api/videos/route.ts
@@ -1,23 +1,21 @@
 import { NextResponse, NextRequest } from "next/server";
-import { chooseFormat, downloadFromInfo, getInfo } from "ytdl-core";
+import {
+  chooseFormat,
+  downloadFromInfo,
+  getInfo,
+  videoFormat,
+} from "ytdl-core";
+import { checkPrams } from "@/app/api/videos/query.schema";
+import { ZodError } from "zod";
 
 interface NextApiResponse extends NextResponse {
   params: { videoName: string };
 }
 
-export async function GET(request: NextRequest, response: NextApiResponse) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id");
-  const quality = searchParams.get("quality");
-  const responseHeaders = new Headers(response.headers);
-  if (!id || !quality) {
-    return NextResponse.json({ data: "id and format is required params" });
-  }
-
+const getHeader = ({ headers }: Response, format: videoFormat) => {
   const randomName = Math.random().toString(36).substring(2, 15);
 
-  const info = await getInfo(id);
-  const format = chooseFormat(info.formats, { quality });
+  const responseHeaders = new Headers(headers);
 
   responseHeaders.set("Content-Type", "video/mp4");
 
@@ -30,10 +28,29 @@ export async function GET(request: NextRequest, response: NextApiResponse) {
     "User-Agent",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
   );
+  return responseHeaders;
+};
 
-  const data = downloadFromInfo(info, { format });
+export async function GET(request: NextRequest, response: NextApiResponse) {
+  try {
+    const { id, quality } = checkPrams(request.url);
 
-  return new Response(data as never, {
-    headers: responseHeaders,
-  });
+    const info = await getInfo(id);
+    const format = chooseFormat(info.formats, { quality });
+
+    const data = downloadFromInfo(info, { format });
+
+    return new Response(data as never, {
+      headers: getHeader(response, format),
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return new NextResponse(error.message, { status: 400 });
+    }
+    if (error instanceof Error) {
+      return new NextResponse(JSON.stringify({ error: error.message }), {
+        status: 400,
+      });
+    }
+  }
 }
