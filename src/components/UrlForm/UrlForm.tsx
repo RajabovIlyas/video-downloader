dc43471e70484ec5b4eb05f489b412427c4ab333
RajabovIlyas <ilyasrajabow@gmail.com>
2024-05-31 16:19:38 +0500

    added button reset in url form

--- src/components/UrlForm/UrlForm.tsx
+++ src/components/UrlForm/UrlForm.tsx
@@ -8,7 +8,7 @@ function UrlForm() {
   return (
     <form onSubmit={onSubmit} className="mt-6 flex gap-1 flex-row">
       <div>
-        <input {...register("url")} className="main-input mb-2" />
+        <input {...register("url")} className="main-input mb-2" type="search" />
         <p className="form-wrong">{errors?.url?.message}</p>
       </div>
       {infoLoading ? (
